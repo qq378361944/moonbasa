5b9a8c8bfe6e1c7b91e563593b060548d9cfac1d
snoob <lypyyah@163.com>
2016-07-22 19:36:35 +0800

    完成了轮播图的1.0版本，有时间继续完善

--- src/js/index.js
+++ src/js/index.js
@@ -26,26 +26,39 @@ $(function() {
 
 /*轮播图*/
 $(function() {
+	//轮播图下的li
 	var lists = $(".bannerp li");
+	//轮播图里面的小圆点
+	var selectLists=$(".select_banner");
 	var index=0;
-	var timer=setInterval(foucs,3000);
+	var timer=setInterval(foucs,2000);
 	function foucs() {
 		if(index >= lists.length) {
 			index = 0;
 		}
-		show();
+		show(index);
 		index++;
 	}
-	function show() {
+	function show(index) {
 		//把当前的li变成block,把其他li变成none
+		var selfindex=index;
 		for(var i = 0; i < lists.length; i++) {
-			if(i == index) {
+			if(i == selfindex) {
 				lists[i].style.display = 'block';
 				lists[i].style.backgroundImage="url(css/img/banner"+(i+1)+".jpg)";
+				selectLists.children("li")[i].style.backgroundImage="url(css/img/redball.png)"
 			} else {
 				lists[i].style.display = 'none';
+				selectLists.children("li")[i].style.backgroundImage="url(css/img/whileball.png)"
 			}
 		}
 	}
-	
+	selectLists.on("mouseover","li",function(e){
+		index=$(this).index();
+		clearInterval(timer);
+		$(this).css("background-image","url(css/img/redball.png)");
+		show(index);
+	}).on("mouseleave","li",function(){
+		timer=setInterval(foucs,2000);
+	});
 });

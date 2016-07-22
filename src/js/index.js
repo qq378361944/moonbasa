977a0a93b24329383c0ffa2082f77b2b78f6f99a
snoob <lypyyah@163.com>
2016-07-22 15:55:52 +0800

    首页静态页面样式基本完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,28 @@
+
 $(function(){
+	/*头部nav鼠标hover特效*/
 	$(".navbox").on("mouseover","img",function(){
 		$(this).attr("src",$(this).attr("src").substring(0,12)+"h.jpg");
 	}).on("mouseleave","img",function(){
 		$(this).attr("src",$(this).attr("src").substring(0,12)+".jpg");
 	});
+
+	/*微博、手机、微信二维图展现特效*/
+	$(".tel").on("mouseover",function(){
+		$(this).children("img").css("display","block");
+	}).on("mouseleave",function(){
+		$(this).children("img").css("display","none");
+	});
+	$(".weibo").on("mouseover",function(){
+		$(this).children("img").css("display","block");
+	}).on("mouseleave",function(){
+		$(this).children("img").css("display","none");
+	});
+		$(".weixin").on("mouseover",function(){
+		$(this).children("img").css("display","block");
+	}).on("mouseleave",function(){
+		$(this).children("img").css("display","none");
+	});
 });
+
+

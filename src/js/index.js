ce05b7bc68aee373a3a7c422ba69db56f6ea1c37
snoob <lypyyah@163.com>
2016-07-22 21:01:18 +0800

    2016年7月22日放学备份

--- src/js/index.js
+++ src/js/index.js
@@ -29,9 +29,10 @@ $(function() {
 	//轮播图下的li
 	var lists = $(".bannerp li");
 	//轮播图里面的小圆点
-	var selectLists=$(".select_banner");
-	var index=0;
-	var timer=setInterval(foucs,2000);
+	var selectLists = $(".select_banner");
+	var index = 0;
+	var timer = setInterval(foucs, 2000);
+
 	function foucs() {
 		if(index >= lists.length) {
 			index = 0;
@@ -39,26 +40,64 @@ $(function() {
 		show(index);
 		index++;
 	}
+
 	function show(index) {
 		//把当前的li变成block,把其他li变成none
-		var selfindex=index;
+		var selfindex = index;
 		for(var i = 0; i < lists.length; i++) {
 			if(i == selfindex) {
 				lists[i].style.display = 'block';
-				lists[i].style.backgroundImage="url(css/img/banner"+(i+1)+".jpg)";
-				selectLists.children("li")[i].style.backgroundImage="url(css/img/redball.png)"
+				lists[i].style.backgroundImage = "url(css/img/banner" + (i + 1) + ".jpg)";
+				selectLists.children("li")[i].style.backgroundImage = "url(css/img/redball.png)"
 			} else {
 				lists[i].style.display = 'none';
-				selectLists.children("li")[i].style.backgroundImage="url(css/img/whileball.png)"
+				selectLists.children("li")[i].style.backgroundImage = "url(css/img/whileball.png)"
 			}
 		}
 	}
-	selectLists.on("mouseover","li",function(e){
-		index=$(this).index();
+	selectLists.on("mouseover", "li", function(e) {
+		index = $(this).index();
 		clearInterval(timer);
-		$(this).css("background-image","url(css/img/redball.png)");
+		$(this).css("background-image", "url(css/img/redball.png)");
 		show(index);
-	}).on("mouseleave","li",function(){
-		timer=setInterval(foucs,2000);
+	}).on("mouseleave", "li", function() {
+		timer = setInterval(foucs, 2000);
 	});
+});
+
+/*黑标导航*/
+$(function() {
+	var navlists = $(".bigbox_right_nav ul li");
+	navlists.on("mouseover", function() {
+		$(this).css({
+			"backgroundColor": "black"
+		}).children("a").css({
+			"color": "white"
+		}).next("span").css({
+			"display": "block"
+		});
+	}).on("mouseleave", function() {
+		$(this).css({
+			"backgroundColor": "white"
+		}).children("a").css({
+			"color": "black"
+		}).next("span").css({
+			"display": "none"
+		});
+	});
+})
+
+/*hover的时候有红色框*/
+$(function() {
+	var alist = $(".bigbox_right_nav2 a");
+	alist.hover(function() {
+		$(this).children("img").css({
+			"border": "1px solid red"
+		});
+	}, function() {
+		$(this).children("img").css({
+			"border": "none"
+		});
+	});
+
 });

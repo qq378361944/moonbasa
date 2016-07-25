82bffe6387ada5aab196e5f57ca7b66abdb4859a
snoob <lypyyah@163.com>
2016-07-25 15:17:18 +0800

    完成banner上的覆盖层

--- src/js/index.js
+++ src/js/index.js
@@ -154,3 +154,11 @@ $(function() {
 		}
 	});
 });
+
+$(function(){
+	$(".list_group").on("mouseover","dl",function(){
+		$(this).css("background-color","#d2005c").children("dd").css("display","block");
+	}).on("mouseleave","dl",function(){
+		$(this).css("background-color","#090909").children("dd").css("display","none");
+	});
+});
